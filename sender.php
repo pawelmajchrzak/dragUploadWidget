53860210e832ab5ecd84fe742b8134b47a9c8073
Pawel Majchrzak <pawel.majchrzak.programista@gmail.com>
2024-12-07 17:15:58 +0100

    Create Database with name of files

--- sender.php
+++ sender.php
@@ -1,10 +1,18 @@
 <?php
+//Include the database configuration file
+require 'dbConfig.php';
+
+
 if(!empty($_FILES['file'])){
     $targetDir = 'uploads/';
     $filename = basename($_FILES['file']['name']);
     $targetFilePath = $targetDir.$filename;
+
+
+    //Upload file to server
     if(move_uploaded_file($_FILES['file']['tmp_name'],$targetFilePath)){
-        echo 'File Uploaded';
+        //Insert file information in the database
+        $insert = $db->query("INSERT INTO files (file_name, uploaded_on) VALUES ('".$filename."', NOW())");
     }
 }
 ?>
